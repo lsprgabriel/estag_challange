9814cae5828afc5d0aa732af175007d4fef2c316
lsprgabriel <ogabrielpereira@pm.me>
2024-02-29 15:59:35 -0300

    adding pdo to orders and items

--- back/src/services/orders.php
+++ back/src/services/orders.php
@@ -4,3 +4,5 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT");
+
+require_once('pdo.php');
