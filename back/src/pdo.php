ff12037088b6b3f1b0675b994b5c82945c0c8b88
lsprgabriel <ogabrielpereira@pm.me>
2024-03-05 14:37:05 -0300

    CHORE: Adding pdo connection to OOP singleton design pattern

--- back/src/pdo.php
+++ back/src/pdo.php
@@ -5,6 +5,20 @@ $db = "applicationphp";
 $user = "root";
 $pw = "root";
 
-define('myDB', new PDO("pgsql:host=$host;dbname=$db", $user, $pw));
+class DB {
+    public function __construct($host, $db, $user, $pw) {
+        $this->host = $host;
+        $this->db = $db;
+        $this->user = $user;
+        $this->pw = $pw;
+    }
 
+    public function connect() {
+        return new PDO("pgsql:host=$this->host;dbname=$this->db", $this->user, $this->pw);
+    }
+}
 
+$myDB = new DB($host, $db, $user, $pw);
+$myDB = $myDB->connect();
+
+define('myDB', $myDB);
