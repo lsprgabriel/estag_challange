caf4e331d5a61c1abebcae6d9801311e53174716
lsprgabriel <ogabrielpereira@pm.me>
2024-03-08 13:44:29 -0300

    fim

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,39 @@
 import { useState } from 'react'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
-  return (
-    <>
-     <h1 className='text-4xl'>Hello World</h1>
-    </>
-  )
+const pages = import.meta.glob("./pages/**/*.jsx", { eager: true });
+
+const routes = [];
+for (const path of Object.keys(pages)) {
+  const fileName = path.match(/\.\/pages\/(.*)\.jsx$/)?.[1];
+  if (!fileName) {
+    continue;
+  }
+
+  const normalizedPathName = fileName.includes("$")
+    ? fileName.replace("$", ":")
+    : fileName.replace(/\/index/, "");
+
+  routes.push({
+    path: fileName === "index" ? "/" : `/${normalizedPathName.toLowerCase()}`,
+    Element: pages[path].default,
+    loader: pages[path]?.loader,
+    action: pages[path]?.action,
+    ErrorBoundary: pages[path]?.ErrorBoundary,
+  });
 }
+
+const router = createBrowserRouter(
+  routes.map(({ Element, ErrorBoundary, ...rest }) => ({
+    ...rest,
+    element: <Element />,
+    ...(ErrorBoundary && { errorElement: <ErrorBoundary /> }),
+  }))
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
